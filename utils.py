91e0e0587de9f5cfd9948b96013ab9f5bf40aef7
Jason Son <jasonson2004@gmail.com>
2024-10-19 03:12:34 -0700

    add: get related details of src node method added

--- utils.py
+++ utils.py
@@ -1,19 +1,20 @@
 # [START utils.py]
-'''
+"""
 This file creates a dependency graph that depicts the relationships between files.
 If the user does not provide a flag, the graph will only create itself from the error stack.
 If the user does provide "-g", the graph will contain all files from the repo, with respect to the .gitignore.
 If the user does provide "-r", the graph will contain all files from the error stack, alongside all files that are imported/included in the source file, and recursively call itself until all relationships are exhausted.
 
-Usage: "splat <?-g> <?-r>"
-'''
+@Usage: "splat <?-g> <?-r>"
+"""
 import os
 from typing import List, Set
+import ast
 
 '''
 This function is a shorthand redirecting based on if a flag is raised during command call or not.
-Usage: call graph_related_files(pure error string, if flag is raised)
-Returns: void
+@Usage: call graph_related_files(pure error string, if flag is raised)
+@Returns: None
 '''
 def graph_related_files(error_info: str, global_flag_raised: bool = False) -> None:
   if global_flag_raised:
@@ -23,8 +24,8 @@ def graph_related_files(error_info: str, global_flag_raised: bool = False) -> No
 
 '''
 This function runs through the entire repository, and dodges all files that do not start with .py.
-Usage: call get_repo_details() or with a specific path (most preferably from the stack error trace)
-Returns: all traced files of type List[str]
+@Usage: call get_repo_details() or with a specific path (most preferably from the stack error trace)
+@Returns: all traced files of type List[str]
 '''
 def get_repo_details(repo_path: str = ".") -> List[str]:
   traced_files = []
@@ -45,6 +46,24 @@ def get_repo_details(repo_path: str = ".") -> List[str]:
 
   return traced_files
 
-''''''
+'''
+This function runs through a source file, and grabs all files linked by any Nth degree connection.
+@Usage: call get_related_details with required parameter: src file path
+@Returns: all files related to parameter file to any Nth degree in type Set[str]
+'''
+def get_related_details(file_path: str) -> Set[str]:
+  with open(file_path, 'r') as file:
+    tree = ast.parse(file.read(), filename=file_path)
+
+  imports = set()
+  for node in ast.walk(tree):
+    if isinstance(node, ast.Import):
+      for alias in node.names:
+        imports.add(alias.name)
+    elif isinstance(node, ast.ImportFrom):
+      if node.module:
+        imports.add(node.module)
+
+  return imports
 
 # [END utils.py]
