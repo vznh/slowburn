9160559485ae7e8f709ff7aca26013672aee37fc
ehcaw <ryannguyenc@gmail.com>
2024-10-19 03:45:47 -0700

    it works only if you type your command in a string ie: 'yarn run dev'

--- errortrace.py
+++ errortrace.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import sys
 
 def get_last_command(file_path):
     try:
@@ -12,14 +13,11 @@ def run_command(command):
     process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     return process.stdout, process.stderr, process.returncode
 
-def splat_find():
-    last_command_path = os.path.expanduser("~/.last_command")
-    last_command = get_last_command(last_command_path)
-
-    if last_command:
-        print(f"Last command was: {last_command}")
+def splat_find(command ):
+    if command:
+        print(f"Last command was: {command}")
         # Add specific logic to process this command
-        stdout, stderr, returncode = run_command(last_command)
-        print(f"{stdout} {stderr} {returncode}")
+        stdout, stderr, returncode = run_command(command)
+        print(f"hello{stdout} bye{stderr} goodbye{returncode}")
     else:
         print("No last command found.")
